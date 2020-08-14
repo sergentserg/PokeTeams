0a3ccd918e22d70ff5be5c43e4212baaa8f51b32
Jose M. Gonzalez <jmgonzalezn01@gmail.com>
2020-08-13 22:27:04 -0400

    Completed pokemon pokedex entry page.

--- dist/js/pokedex.js
+++ dist/js/pokedex.js
@@ -1,63 +1,75 @@
-// Default spread is pokedex national order
-// Grab Sprite - use as bg-image for card
-// Grab pokemon typage
-// Grab pokemon name
-
-const MAX_DEX_ID = 807;
-const POKE_PER_PAGE = 20;
-const PAGINATION_LIMIT = 5;
 window.addEventListener("load", (e) => {
-  // Clear current results from session storage.
-  if (sessionStorage.getItem("currentResults")) {
-    sessionStorage.removeItem("currentResults");
-  }
-  // Do not request again on refresh; save result once to storage.
   if (!localStorage.getItem("allPokemon")) {
     getAsync(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_DEX_ID}`).then(
       (allPokemon) => {
-        // Save 'Pokedex' to local storage.
-        localStorage.setItem("allPokemon", JSON.stringify(allPokemon));
+        // Update name and dexID for all pokemon.
+        allPokemon.results.forEach((pokemon, index) => {
+          pokemon.dexID = pokemon.url.split("/")[6];
+          pokemon.dexID = ("000" + pokemon.dexID).substr(-3, 3);
+          pokemon.name = capitalizeWord(pokemon.name);
+        })
+
+        // Display the first 20 pokemon.
         for (let i = 0; i < POKE_PER_PAGE; i++) {
           displayPokemonCard(allPokemon.results[i]);
         }
-        // Display the pagination, starting at page 1.
-        createPagination(allPokemon.results);
-      }
-    );
-  } else {
-    // Already in local storage; retrieve it.
-    console.log("already saved!");
-    const allPokemon = JSON.parse(localStorage.getItem("allPokemon"));
 
-    for (let i = 0; i < POKE_PER_PAGE; i++) {
-      displayPokemonCard(allPokemon.results[i]);
+        // Display the pagination.
+        createPagination(allPokemon.results);
+
+        // Save 'Pokedex' to local storage.
+        localStorage.setItem("allPokemon", JSON.stringify(allPokemon));
+      });
+  } else {
+    // Some results are already in storage; retrieve them.
+    const results = getResultsArray();
+    const searchQuery = getResultsQuery();
+
+    for (let i = 0; i < Math.min(results.length, POKE_PER_PAGE); i++) {
+      displayPokemonCard(results[i]);
     }
+
     // Display the pagination.
-    createPagination(allPokemon.results);
+    createPagination(results);
+
+    if (searchQuery) {
+      // Render search query as a visible button
+      const filterContainer = document.querySelector(".filter-list");
+      filterContainer.innerHTML = `
+    <button class="mt-1 btn btn-dark filter-tag">"${searchQuery}" <i class="fas fa-times-circle fa-xs"></i>
+    </button>`
+      filterContainer.querySelector(".filter-tag").addEventListener("click", removeFilter);
+    }
   }
 });
 
-async function getAsync(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw Error(`Failed to get response from ${url}.`);
-    }
-    return await response.json();
-  } catch (e) {
-    console.log(e);
-  }
-}
+// async function getAsync(url) {
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw Error(`Failed to get response from ${url}.`);
+//     }
+//     return await response.json();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
 
 const searchPokemon = (event) => {
+  // console.log("Search Pokemon Function has run.")
   // Search Filter!
-  const searchQuery = document.querySelector("#pokemonSearchInput").value;
+  const searchQuery = document.querySelector("#pokemonSearchInput").value.toLowerCase();
+  const allPokemon = JSON.parse(localStorage.getItem("allPokemon"));
+  const resultsContainer = document.querySelector("#pokeSearchResults");
   if (searchQuery !== "") {
-    const allPokemon = JSON.parse(localStorage.getItem("allPokemon"));
+    // Clear current results from session storage.
+    if (sessionStorage.getItem("currentResults")) {
+      sessionStorage.removeItem("currentResults");
+    }
 
     // Get list of Pokemon matching search query.
     const results = allPokemon.results.filter(
-      (pokemon) => pokemon.name.indexOf(searchQuery) != -1
+      (pokemon) => pokemon.name.toLowerCase().indexOf(searchQuery) != -1
     );
 
     // Save results to session storage.
@@ -68,7 +80,6 @@ const searchPokemon = (event) => {
     sessionStorage.setItem("currentResults", JSON.stringify(currentResults));
 
     // Clean out old Search results
-    const resultsContainer = document.querySelector("#pokeSearchResults");
     while (resultsContainer.firstElementChild) {
       resultsContainer.firstElementChild.remove();
     }
@@ -80,28 +91,33 @@ const searchPokemon = (event) => {
 
     createPagination(results);
 
+    // Render search query as a visible button
+    const filterContainer = document.querySelector(".filter-list");
+    // Clear filter container
+    while (filterContainer.firstElementChild) {
+      filterContainer.firstElementChild.remove();
+    }
+    filterContainer.innerHTML = `
+    <button class="mt-1 btn btn-dark filter-tag">"${searchQuery}" <i class="fas fa-times-circle fa-xs"></i>
+    </button>`
+    filterContainer.querySelector(".filter-tag").addEventListener("click", removeFilter);
+    // const filterTag = document.createElement('button');
+    // filterTag.classList.add('mt-1', 'btn', 'btn-dark', 'filter-tag');
+    // filterTag.innerHTML = `"${searchQuery}" <i class="fas fa-times-circle fa-xs"></i>`;
+    // filterTag.addEventListener("click", removeFilter);
+    // filterContainer.appendChild(filterTag);
+
     // Do not reload page.
     event.preventDefault();
   }
 };
 
 const displayPokemonCard = (pokemon) => {
-  const resultsContainer = document.querySelector("#pokeSearchResults");
-
-  // Assign Pokedex DexID as an attribute.
-  pokemon.dexID = pokemon.url.split("/")[6];
-  // Append 0s to Pokemon DexID until total of 3 digits
-  while (pokemon.dexID.length < 3) {
-    pokemon.dexID = "0" + pokemon.dexID;
-  }
-
-  // Capitalize pokemon name
-  pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-
-  // Display the Pokemon card.
-  resultsContainer.innerHTML += `
-  <div class="pokemon-card border">
-    <select class="form-control" name="teamSelect">
+  // Create pokemon card.
+  const pokemonCard = document.createElement('div');
+  pokemonCard.classList.add('pokemon-card', 'border');
+  pokemonCard.innerHTML = `
+  <select class="form-control" name="teamSelect">
       <option selected disabled hidden>Choose a team</option>
       <option value="teamA">Team A</option>
       <option value="teamB">Team B</option>
@@ -111,7 +127,7 @@ const displayPokemonCard = (pokemon) => {
       class="poke-img d-flex justify-content-center align-content-center py-4"
     >
       <img
-        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemon.dexID}.png"
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemon.dexID}.png" alt="${pokemon.name}"
       />
       <div class="poke-card-buttons d-flex justify-content-between">
         <button class="btn poke-add-btn">
@@ -123,11 +139,17 @@ const displayPokemonCard = (pokemon) => {
       </div>
     </div>
 
-    <button class="btn btn-block btn-dark poke-details-btn">
-       ${pokemon.dexID} ${pokemon.name}
+    <button class="btn btn-block btn-dark poke-details-btn" data-poke-id="${parseInt(pokemon.dexID)}">
+      #${pokemon.dexID} ${pokemon.name}
       <i class="fas fa-eye"></i>
-    </button>
-  </div>`;
+    </button>`;
+  pokemonCard.querySelector('.poke-details-btn').addEventListener("click", () => {
+    // Passing in json index of pokemon
+    getPokemon(parseInt(pokemon.dexID) - 1);
+  });
+
+  const resultsContainer = document.querySelector("#pokeSearchResults");
+  resultsContainer.appendChild(pokemonCard);
 };
 
 // If totalPages == 1, pagination is not needed
@@ -168,7 +190,7 @@ const createPagination = (
       li.setAttribute(`data-page-num`, `${newFirstPageNum + offset}`);
       li.innerHTML = `<button href="#" class="page-link">${
         newFirstPageNum + offset
-      }</button>`;
+        }</button>`;
 
       // Insert it before next.
       pagination.insertBefore(li, nextLi);
@@ -233,7 +255,6 @@ const createPagination = (
 };
 
 const displayNewPage = (event) => {
-  console.log(event.target.innerHTML);
   updatePagination(event);
   const results = getResultsArray();
   // Clean out old Search results
@@ -258,13 +279,20 @@ const displayNewPage = (event) => {
 
 const getResultsArray = () => {
   const currentResults = JSON.parse(sessionStorage.getItem("currentResults"));
-  // pokemonData is the entire pokedex array or a filtered array.
+  // the entire pokedex array or a filtered array.
   if (currentResults) {
-    // Compute totalPages and pokemonData from current results
     return currentResults.results;
   } else {
-    // Compute totalPages and pokemonData from original results
     return JSON.parse(localStorage.getItem("allPokemon")).results;
+  }
+};
+
+const getResultsQuery = () => {
+  const currentResults = JSON.parse(sessionStorage.getItem("currentResults"));
+  if (currentResults) {
+    return currentResults.searchQuery;
+  } else {
+    return null;
   }
 };
 
@@ -323,13 +351,69 @@ const updatePagination = (event) => {
   createPagination(pokemonData, newFirstPageNum, newActivePageNum);
 };
 
-const getPokemon = (pokemon) => {
-  let url = pokemon.url;
+// const getPokemon = (event) => {
+//   let btn = event.target;
+//   while (!btn.classList.contains('poke-details-btn'))
+//     btn = btn.parentElement;
 
-  getAsync(url).then((pokemonData) => {
-    console.log(pokemonData);
-  });
-};
+//   const pokedexID = parseInt(btn.getAttribute("data-poke-id"));
+//   const pokeIndex = pokedexID - 1;
+
+//   const allPokemon = JSON.parse(localStorage.getItem('allPokemon'));
+//   const currentPokemon = allPokemon.results[pokeIndex];
+
+//   // Provide access to next/previous pokemon from current.
+
+//   const next = pokedexID < MAX_DEX_ID ? allPokemon.results[pokeIndex + 1] : null;
+//   const previous = pokedexID > 1 ? allPokemon.results[pokeIndex - 1] : null;
+
+//   getAsync(currentPokemon.url).then((pokemonData) => {
+//     const { abilities, height, id, sprites, stats, types, weight } = pokemonData;
+
+//     const data = {
+//       // base_experience: pokemonData.base_experience,
+//       abilities,
+//       height: height / 10,
+//       id,
+//       dexID: currentPokemon.dexID,
+//       moves: getGen7Moves(pokemonData.moves), // Three arrays into property object
+//       name: currentPokemon.name,
+//       sprites,
+//       stats,
+//       types,
+//       weight: weight / 10,
+//       next: next,
+//       previous: previous
+//     };
+//     localStorage.setItem('currentPokemon', JSON.stringify(data));
+//     window.location.href = "pokemon.html"
+//   });
+// };
+
+
+const removeFilter = (event) => {
+  // Clear current results from session storage.
+  if (sessionStorage.getItem("currentResults")) {
+    sessionStorage.removeItem("currentResults");
+  }
+  const results = getResultsArray(); // ATM returns allPokemon results
+  const resultsContainer = document.querySelector("#pokeSearchResults");
+  // Reset search form
+  document.querySelector(".search-form").reset();
+  // Remove filter tag button element
+  document.querySelector(".filter-tag").remove();
+
+  // Clean out old Search results
+  while (resultsContainer.firstElementChild) {
+    resultsContainer.firstElementChild.remove();
+  }
+  // Display Pokemon cards
+  for (let i = 0; i < Math.min(results.length, POKE_PER_PAGE); i++) {
+    displayPokemonCard(results[i]);
+  }
+  // Display the pagination.
+  createPagination(results);
+}
 
 document
   .querySelector("#pokemonSearchBtn")
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,22 @@
+// Default spread is pokedex national order
+// Grab Sprite - use as bg-image for card
+// Grab pokemon typage
+// Grab pokemon name
+
+const MAX_DEX_ID = 807;
+const POKE_PER_PAGE = 20;
+// Temporarily switched to let
+let PAGINATION_LIMIT = 5;
+
+// Testing media query in JS
+const mobileMediaQuery = window.matchMedia("(max-width: 576px)");
+if (mobileMediaQuery.matches) {
+  PAGINATION_LIMIT = 1;
+}
+else {
+  PAGINATION_LIMIT = 5;
+}
+
 shrinkSidenav = () => {
   const shrinkBtn = document.querySelector(".shrink-btn");
   const expandBtn = document.querySelector(".expand-btn");
@@ -34,4 +53,97 @@ openSidenav = () => {
   expandBtn.style.display = "none";
   // mainPage.style.backgroundColor = "rgba(0,0,0,0.1)";
   mainPage.style.opacity = "0.2";
+};
+
+// =============================================================================
+// Utility functions
+// =============================================================================
+const capitalizeWord = (word) => {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+async function getAsync(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw Error(`Failed to get response from ${url}.`);
+    }
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const getGen7Moves = (pokemonMoves) => {
+  const egg = [], machine = [], levelUp = [];
+
+  pokemonMoves.forEach(moveData => {
+    const versions = moveData.version_group_details;
+
+    // Determine if the move is available in gen 7.
+    const gen7Move = versions.filter(version =>
+      version.version_group.name === "ultra-sun-ultra-moon");
+
+    if (gen7Move.length > 0) {
+      const method = gen7Move[0].move_learn_method.name;
+      const move = moveData.move;
+      switch (method) {
+        case "egg":
+          egg.push({ ...move, method });
+          break;
+        case "machine":
+          machine.push({ ...move, method });
+          break;
+        case "level-up":
+          levelUp.push({ ...move, method, level: gen7Move[0].level_learned_at });
+          break;
+      }
+    }
+  })
+  return { egg, machine, levelUp };
+}
+
+const getPokemon = (pokeIndex) => {
+  const allPokemon = JSON.parse(localStorage.getItem("allPokemon"));
+  const currentPokemon = allPokemon.results[pokeIndex];
+
+  let next = null;
+  if (pokeIndex < MAX_DEX_ID - 1) {
+    next = {
+      name: allPokemon.results[pokeIndex + 1].name,
+      dexID: allPokemon.results[pokeIndex + 1].dexID,
+      url: allPokemon.results[pokeIndex + 1].url
+    }
+  }
+
+  let previous = null;
+  if (pokeIndex > 0) {
+    previous = {
+      name: allPokemon.results[pokeIndex - 1].name,
+      dexID: allPokemon.results[pokeIndex - 1].dexID,
+      url: allPokemon.results[pokeIndex - 1].url
+    }
+  }
+
+  getAsync(currentPokemon.url).then((pokemonData) => {
+    const { abilities, height, id, sprites, stats, types, weight } = pokemonData;
+
+    const data = {
+      // base_experience: pokemonData.base_experience,
+      abilities,
+      height: height / 10,
+      id,
+      dexID: currentPokemon.dexID,
+      moves: getGen7Moves(pokemonData.moves),
+      name: currentPokemon.name,
+      sprites,
+      stats,
+      types,
+      weight: weight / 10,
+      next,
+      previous
+    };
+    localStorage.setItem('currentPokemon', JSON.stringify(data));
+    window.location.href = "pokemon.html"
+  });
 };
