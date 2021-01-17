4552d60b5c22cbbcd8ab1bcd7e23c21a707a7f47
Sergio Garcia <gsergio488@gmail.com>
2021-01-17 02:42:18 -0500

    Created MongoDB connection for app database
    Introduced Mongoose to rely on the ORM features it offers.
    Also, introduced the morgan logger middleware and colors
    for prettier console output.

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const teams = require('./routes/teams');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: 'rest_api/config/config.env' });
 
+// Route files
+const teams = require('./routes/teams');
+
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/teams', teams);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.magenta
+      .bold
+  )
 );
+
+// Unhandled Promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process.
+  server.close(() => process.exit(1));
+});
