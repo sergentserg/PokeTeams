722f6f19e3c0d7cef5bd1e5460faf65e949f3fa9
Sergio Garcia <gsergio488@gmail.com>
2021-01-17 14:09:01 -0500

    Introduced asyncHandler to avoid repeat try-catch
    The asyncHandler middleware wraps the async route
    controller methods to avoid repeating the try-catch
    blocks that help in handling errors.

--- rest_api/controllers/teams.js
+++ rest_api/controllers/teams.js
@@ -1,93 +1,71 @@
 const Team = require('../models/Team');
+const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc      Get All Teams
 // @route     GET /api/v1/teams
 // @access    Private
-exports.getTeams = async (req, res, next) => {
-  try {
-    const teams = await Team.find();
-
-    res.status(200).json({ success: true, data: teams, count: teams.length });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getTeams = asyncHandler(async (req, res, next) => {
+  const teams = await Team.find();
+  res.status(200).json({ success: true, data: teams, count: teams.length });
+});
 
 // @desc      Get single Team
 // @route     GET /api/v1/teams/:id
 // @access    Private
-exports.getTeam = async (req, res, next) => {
-  try {
-    const team = await Team.findById(req.params.id);
-
-    // Correctly formatted ID: no result found. End cycle via return.
-    if (!team) {
-      return next(
-        new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: team });
-  } catch (err) {
-    // Incorrectly formatted ID.
-    next(err);
+exports.getTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.findById(req.params.id);
+  // Correctly formatted ID: no result found. End cycle via return.
+  if (!team) {
+    return next(
+      new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: team });
+});
 
 // @desc      Create a single Team
 // @route     POST /api/v1/teams/:id
 // @access    Private
-exports.createTeam = async (req, res, next) => {
-  try {
-    const team = await Team.create(req.body);
+exports.createTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: team,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: team,
+  });
+});
 
 // @desc      Update Team
 // @route     POST /api/v1/teams/:id
 // @access    Private
-exports.updateTeam = async (req, res, next) => {
-  try {
-    const team = await Team.findByIdAndUpdate(req.params.id, req.body, {
-      // team is the new (updated) data from the body.
-      new: true,
-      runValidators: true,
-    });
+exports.updateTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.findByIdAndUpdate(req.params.id, req.body, {
+    // team is the new (updated) data from the body.
+    new: true,
+    runValidators: true,
+  });
 
-    if (!team) {
-      return next(
-        new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true, data: team });
-  } catch (err) {
-    next(err);
+  if (!team) {
+    return next(
+      new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: team });
+});
 
 // @desc      Delete Team
 // @route     DELETE /api/v1/teams/:id
 // @access    Private
-exports.deleteTeam = async (req, res, next) => {
-  try {
-    const team = await Team.findByIdAndDelete(req.params.id);
+exports.deleteTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.findByIdAndDelete(req.params.id);
 
-    if (!team) {
-      return next(
-        new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
+  if (!team) {
+    return next(
+      new ErrorResponse(`Team not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
