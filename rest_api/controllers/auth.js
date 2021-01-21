7f3ae01f623d46728d9bc7d626ac84754b86b438
Sergio Garcia <gsergio488@gmail.com>
2021-01-20 19:34:13 -0500

    JWT returned upon successful POST to /auth route
    Mongoose method takes care of signing a JWT to be
    returned to client upon successful registration
    or login. A succesful POST to /auth/login will
    contain a correct email and password, checked
    against the hashed password in database.

--- rest_api/controllers/auth.js
+++ rest_api/controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -15,5 +15,43 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
   });
 
-  res.status(200).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password.
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user; explicitly requests password (defaults to not returning it).
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(
+      new ErrorResponse('The credentials provided were invalid.', 401)
+    );
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(
+      new ErrorResponse('The credentials provided were invalid.', 401)
+    );
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
