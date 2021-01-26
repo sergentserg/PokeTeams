c758236b14917e2b9bf2b3a5612b499827fa570b
Sergio Garcia <gsergio488@gmail.com>
2021-01-26 12:29:58 -0500

    Implemented password reset functionality
    A POST request to the /forgotpassword endpoint with an
    email in the body will send an email with a token
    (or URL) for resetting a password in the database which
    is valid for 10 mins.

--- rest_api/models/User.js
+++ rest_api/models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -23,7 +24,7 @@ const UserSchema = new mongoose.Schema({
     select: false,
   },
   resetPasswordToken: String,
-  resetPasswordExpired: Date,
+  resetPasswordExpire: Date,
   createdAt: {
     type: Date,
     default: Date.now,
@@ -32,6 +33,10 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt user's password with bcryptjs.
 UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -46,6 +51,23 @@ UserSchema.methods.getSignedJwtToken = function () {
 // Match user entered password to hashed password in database.
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token.
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate the token.
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field.
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set the expire field. (10 minutes)
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model('User', UserSchema);
